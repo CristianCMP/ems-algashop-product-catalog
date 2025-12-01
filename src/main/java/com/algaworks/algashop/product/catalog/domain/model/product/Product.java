fab95a50601ed390772e1687f77688668efd8696
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-12-01 16:37:38 -0300

    - Mapping Product with Document

--- src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/Product.java
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/Product.java
@@ -1,4 +1,60 @@
 package com.algaworks.algashop.product.catalog.domain.model.product;
 
+import com.algaworks.algashop.product.catalog.domain.model.IdGenerator;
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Document(collection = "products")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Product {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private String name;
+
+    private String brand;
+
+    private String description;
+
+    private Integer quantityInStock;
+
+    private Boolean enabled;
+
+    private BigDecimal regularPrice;
+
+    private BigDecimal salePrice;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    private OffsetDateTime updatedAt;
+
+    @CreatedBy
+    private UUID createdByUserId;
+
+    @LastModifiedBy
+    private UUID lastModifiedByUserId;
+
+    @Builder
+    public Product(String brand, String description, Boolean enabled, BigDecimal regularPrice, BigDecimal salePrice) {
+        this.id = IdGenerator.generateTimeBasedUUID();
+        this.brand = brand;
+        this.description = description;
+        this.enabled = enabled;
+        this.regularPrice = regularPrice;
+        this.salePrice = salePrice;
+    }
 }
