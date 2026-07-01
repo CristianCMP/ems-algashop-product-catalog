8c69f468cbb8772bc2457401636f4a2fad3aa508
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-07-01 15:26:11 -0300

    - Using domain service to update stock

---
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/ProductRestockedEvent.java
@@ -0,0 +1,18 @@
+package com.algaworks.algashop.product.catalog.domain.model.product;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+@ToString
+public class ProductRestockedEvent {
+    private UUID productId;
+
+    @Builder.Default
+    private OffsetDateTime restockedAt = OffsetDateTime.now();
+}
--- src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/QuantityInStockAdjustment.java
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/QuantityInStockAdjustment.java
@@ -10,5 +10,13 @@ public interface QuantityInStockAdjustment {
             UUID productId,
             int previousQuantity,
             int newQuantity
-    ){}
+    ) {
+        public boolean isOutOfStock() {
+            return newQuantity == 0 && previousQuantity != 0;
+        }
+
+        public boolean isRestocked() {
+            return newQuantity > 0 && previousQuantity == 0;
+        }
+    }
 }
