5be534e1411ec543dcea9d81997633b24493c622
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-07-07 15:18:16 -0300

    - HTTP Caching with Spring Framework

--- src/main/java/com/algaworks/algashop/product/catalog/application/product/query/ProductDetailOutput.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/product/query/ProductDetailOutput.java
@@ -30,4 +30,8 @@ public class ProductDetailOutput {
 
     private Integer quantityInStock;
     private Integer discountPercentageRounded;
+
+    private Long version;
+
+    private OffsetDateTime updatedAt;
 }
