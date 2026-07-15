c8cf1d7e7b6461e8ea1e777f6700cf88b6ec45ca
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-07-15 00:26:20 -0300

    - Caching: Understanding the use of eventual consistency or programmatic purging

--- src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryQueryService.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryQueryService.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.Cacheable;
 import java.util.UUID;
 
 public interface CategoryQueryService {
-    @Cacheable(cacheNames = "algashop:categories-filter:v1",key = "'default'", condition = "#filter.isCacheable()")
+    @Cacheable(cacheNames = "algashop:categories-filter:v1", key = "'default'", condition = "#filter.isCacheable()")
     PageModel<CategoryDetailOutput> filter(CategoryFilter filter);
     @Cacheable(cacheNames = "algashop:categories:v1", key = "#categoryId")
     CategoryDetailOutput findById(UUID categoryId);
